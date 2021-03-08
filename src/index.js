6fd01d1c8de971ca1fa7a4fbdb76b334b564058c
Pavel <pl.hancharou@gmail.com>
2021-03-08 14:25:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = bracketsConfig.map(item => item.join(''));
+  let string = str,
+    val;
+
+  do {
+    val = string;
+    arr.forEach(item => string = string.replace(item, ''));
+  } while (string.length < val.length)
+
+  return !string.length;
 }
